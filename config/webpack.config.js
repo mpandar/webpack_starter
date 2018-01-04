4ca6a54c89e82e45b5901f2e771c1c37f8c9e45d
ipanda <mshp_2007@126.com>
2018-01-04 08:16:43 +0800

    add postcss babel ...

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -7,17 +7,81 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(base, 'dist')
   },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(base, 'dist'),
+    historyApiFallback: true,
+    inline: true,
+    proxy: {
+      "/api": "http://localhost:8000"
+    }
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: path.resolve(base, 'config', 'postcss.config.js')
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader",
+          options: {
+            modules: false,
+            localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            minimize: false
+          }
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            config: {
+              path: path.resolve(base, 'config', 'postcss.config.js')
+            }
+          }
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              fallback: 'file-loader',
+              name: '[hash:5].[ext]',
+              // publicPath: 'https://cdn.j2do.com/',
+              outputPath: 'images/'
+            }
+          }
+        ]
       }
     ]
   },
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+// import style from './css/main.css'
+import style from './sass/main.scss'
 let es6 = () => {
   console.log('run in es6')
+  console.log(style)
 }
 es6();
